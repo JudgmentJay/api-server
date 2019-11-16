92361717a03abe875804ac0cca3a485e74816d69
Jay Posten <jdposten@gmail.com>
2019-11-16 12:09:00 -0600

    Smarter queries

--- routers/games.js
+++ routers/games.js
@@ -5,6 +5,90 @@ const gamesRouter = express.Router()
 
 const password = '$$jaysnewtabpassword$$'
 
+const generateInsertQuery = (type, body) => {
+	const validFields = {
+		game: [
+			'title',
+			'status',
+			'releaseDate',
+			'score',
+			'playthroughCount'
+		],
+		playthrough: [
+			'dateStarted',
+			'dateFinished',
+			'hoursPlayed',
+			'platform'
+		]
+	}
+
+	const numberFields = [
+		'score',
+		'playthroughCount',
+		'hoursPlayed'
+	]
+
+	const bodyData = Object.entries(body)
+
+	const fieldsToAdd = []
+
+	bodyData.forEach((param) => {
+		if (validFields[type].includes(param[0]) && param[1] !== '') {
+			fieldsToAdd.push({
+				field: param[0],
+				value: numberFields.includes[param[0]] ? parseInt(param[1]) : param[1]
+			})
+		}
+	})
+
+	const fields = fieldsToAdd.map((field) => field.field).join(', ')
+	const values = fieldsToAdd.map((field) => {
+		return typeof field.value === 'string' ? `'${field.value}'` : field.value
+	}).join(', ')
+
+	return {fields, values}
+}
+
+const generateUpdateQuery = (type, body) => {
+	const validFields = {
+		gamePlaythrough: [
+			'dateStarted',
+			'dateFinished',
+			'hoursPlayedList'
+		]
+	}
+
+	const numberFields = [
+		'score',
+		'playthroughCount',
+		'hoursPlayed'
+	]
+
+	const bodyData = Object.entries(body)
+
+	const fieldsToAdd = []
+
+	bodyData.forEach((param) => {
+		if (validFields[type].includes(param[0]) && param[1] !== '') {
+			if (type === 'gamePlaythrough') {
+				if (param[0] === 'dateStarted') { param[0] = 'lastDateStarted' }
+				if (param[0] === 'dateFinished') { param[0] = 'lastDateFinished' }
+			}
+
+			fieldsToAdd.push({
+				field: param[0],
+				value: numberFields.includes[param[0]] ? parseInt(param[1]) : param[1]
+			})
+		}
+	})
+
+	const setString = fieldsToAdd.map((field) => {
+		return `${field.field} = ${typeof field.value === 'string' ? `'${field.value}'` : field.value }`
+	}).join(', ')
+
+	return setString
+}
+
 gamesRouter.get('/read', (req, res) => {
 	const db = new sqlite3.Database('./games.sqlite', error => {
 		if (error) {
@@ -12,7 +96,7 @@ gamesRouter.get('/read', (req, res) => {
 		}
 	})
 
-	db.all(`SELECT rowid, * FROM games ORDER BY title`, (error, games) => {
+	db.all(`SELECT rowid AS id, * FROM games ORDER BY title`, (error, games) => {
 		if (error) {
 			console.log(`Error retrieving games: ${error}`)
 			res.status(400).send(`Error retrieving games: ${error}`)
@@ -31,7 +115,7 @@ gamesRouter.get('/read', (req, res) => {
 				}
 
 				if (game.status === 'played' || game.status === 'dropped' || game.playthroughCount > 0) {
-					game.hoursPlayed = JSON.parse(game.hoursPlayed)
+					game.hoursPlayedList = JSON.parse(game.hoursPlayedList)
 
 					response.games.played.push(game)
 				}
@@ -57,7 +141,7 @@ gamesRouter.get('/playthroughs/:gameId', (req, res) => {
 		}
 	})
 
-	db.all(`SELECT rowid, * FROM playthroughs WHERE gameId = ${gameId} ORDER BY dateStarted`, (error, playthrough) => {
+	db.all(`SELECT rowid AS id, * FROM playthroughs WHERE gameId = ${gameId} ORDER BY dateStarted`, (error, playthrough) => {
 		if (error) {
 			console.log(`Error retrieving games: ${error}`)
 			res.status(400).send(`Error retrieving games: ${error}`)
@@ -73,20 +157,7 @@ gamesRouter.post('/add', (req, res) => {
 	if (req.body.password !== password) {
 		res.status(400).send()
 	} else {
-		let title = req.body.title
-
-		if (title.includes('\'')) {
-			title = title.replace(/'/g, '\'\'')
-		}
-
-		const {
-			status,
-			releaseDate,
-			playthroughCount,
-			addPlaythroughData
-		} = req.body
-
-		const score = req.body.score !== 0 ? req.body.score : null
+		const query = generateInsertQuery('game', req.body)
 
 		const db = new sqlite3.Database('./games.sqlite', error => {
 			if (error) {
@@ -94,31 +165,26 @@ gamesRouter.post('/add', (req, res) => {
 			}
 		})
 
-		db.run(`INSERT INTO games (title, releaseDate, score, playthroughCount, status) VALUES ('${title}', '${releaseDate}', ${score}, ${playthroughCount}, '${status}')`, function(error) {
+		db.run(`INSERT INTO games (${query.fields}) VALUES (${query.values})`, function(error) {
 			if (error) {
 				console.log(`Error inserting new game: ${error}`)
 				res.status(400).send(`Error inserting new game: ${error}`)
 			} else {
-				if (addPlaythroughData) {
+				if (req.body.addPlaythroughData) {
 					const gameId = this.lastID
 
-					const {
-						dateStarted,
-						dateFinished,
-						hoursPlayed,
-						platform,
-					} = req.body
+					const query = generateInsertQuery('playthrough', req.body)
 
-					const hoursPlayedList = req.body.hoursPlayedList !== '' ? req.body.hoursPlayedList : null
-
-					db.run(`INSERT INTO playthroughs (gameId, dateStarted, dateFinished, hoursPlayed, platform) VALUES (${gameId}, '${dateStarted}', '${dateFinished}', ${hoursPlayed}, '${platform}')`, function(error) {
+					db.run(`INSERT INTO playthroughs (gameId, ${query.fields}) VALUES (${gameId}, ${query.values})`, function(error) {
 						if (error) {
 							console.log(`Error inserting new playthrough: ${error}`)
 							res.status(400).send(`Error inserting new playthrough: ${error}`)
 						} else {
 							const playthroughId = this.lastID
 
-							db.run(`UPDATE games SET lastDateStarted = '${dateStarted}', lastDateFinished = '${dateFinished}', hoursPlayed = '${hoursPlayedList}', lastPlaythroughId = ${playthroughId} WHERE rowid = ${gameId}`, error => {
+							const setString = generateUpdateQuery('gamePlaythrough', req.body)
+
+							db.run(`UPDATE games SET lastPlaythroughId = ${playthroughId}, ${setString} WHERE rowid = ${gameId}`, error => {
 								if (error) {
 									console.log(`Error updating game with new dates: ${error}`)
 									res.status(400).send(`Error updating game with new dates: ${error}`)
