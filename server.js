ddac8ec712cb0fc30e422e190d867b84419b0c8a
Jay Posten <jdposten@gmail.com>
2020-02-01 11:17:58 -0600

    Adding cors-anywhere support

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const helmet = require('helmet')
 const compression = require('compression')
+const cors = require('cors-anywhere')
 
 const bookmarksRouter = require('./routers/bookmarks')
 const gamesRouter = require('./routers/games')
@@ -11,11 +12,28 @@ server.use(helmet())
 server.use(compression())
 server.use(express.json())
 
+server.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+	res.header('Access-Control-Allow-Headers', 'Content-Type')
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+
+	next()
+})
+
+const proxy = cors.createServer({
+	originWhitelist: []
+})
+
+server.get('/proxy/:proxyUrl*', (req, res) => {
+	req.url = req.url.replace('/proxy/', '/')
+	proxy.emit('request', req, res)
+})
+
 server.use('/bookmarks', bookmarksRouter)
 server.use('/games', gamesRouter)
 
-const PORT = 3010
+const port = 3010
 
-server.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`)
+server.listen(port, () => {
+	console.log(`Running API server on port ${port}`)
 })
